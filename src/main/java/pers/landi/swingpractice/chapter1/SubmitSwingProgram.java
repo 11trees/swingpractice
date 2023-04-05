eca1a72c2def06da84e279314876fb4b2ed08d14
landi <landi1996@me.com>
2023-04-05 18:02:00 +0800

    practice: complete practice 1.3

--- src/main/java/pers/landi/swingpractice/chapter1/SubmitSwingProgram.java
+++ src/main/java/pers/landi/swingpractice/chapter1/SubmitSwingProgram.java
@@ -1,5 +1,7 @@
 package pers.landi.swingpractice.chapter1;
 
+import pers.landi.swingpractice.util.SwingConsole;
+
 import javax.swing.*;
 
 public class SubmitSwingProgram extends JFrame {
@@ -17,18 +19,9 @@ public class SubmitSwingProgram extends JFrame {
     static SubmitSwingProgram ssp;
 
     public static void main(String[] args) throws Exception {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                ssp = new SubmitSwingProgram();
-            }
-        });
+        ssp = new SubmitSwingProgram();
+        SwingConsole.run(ssp, 300, 100);
         Thread.sleep(5000);
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                ssp.label.setText("Hey! This is Different!");
-            }
-        });
+        SwingUtilities.invokeLater(() -> ssp.label.setText("Hey! This is Different!"));
     }
 }
---
+++ src/main/java/pers/landi/swingpractice/util/SwingConsole.java
@@ -0,0 +1,13 @@
+package pers.landi.swingpractice.util;
+
+import javax.swing.*;
+public class SwingConsole {
+    public static void run(final JFrame f, final int width, final int height) {
+        SwingUtilities.invokeLater(() -> {
+            f.setTitle(f.getClass().getSimpleName());
+            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            f.setSize(width, height);
+            f.setVisible(true);
+        });
+    }
+}
